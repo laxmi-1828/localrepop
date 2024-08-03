6ca108e54ab4cbf7c7eb385c5bbbfad301719bda
Laxmi Maurya <laxmimaurya52@gmail.com>
2024-08-02 17:16:36 -0700

    initial file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    echo "Hello I'am Laxmi .";
+ ?>
+<html>
+    <head>
+        Myproject git
+    </head>
+    <link rel="stylesheet" href="style.css">
+    <body>
+        <h1 class="heading">Git</h1>
+    </body>
+</html>
+
